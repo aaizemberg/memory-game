9085887fb34502d31b336d27526856433d722f04
Ariel Aizemberg <ariel.aizemberg@gmail.com>
2024-10-30 15:11:13 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['wttsimfiles.blob.core.windows.net'
-        // ,'live.staticflickr.com'   // si quiero agregar las fotos de flickr
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'wttsimfiles.blob.core.windows.net'        
+      } //,
+      // {
+      //   protocol: 'https',
+      //   hostname: 'live.staticflickr.com', // Si quiero agregar las fotos de flickr
+      // },
+    ],
+  },
+};
+
+export default nextConfig;
