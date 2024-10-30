b89a71a9b20aabcbb366ac848d01c9c02fec4e15
Ariel Aizemberg <ariel.aizemberg@gmail.com>
2024-10-30 13:46:18 -0300

    adding more sources

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['wttsimfiles.blob.core.windows.net'],
+      domains: ['wttsimfiles.blob.core.windows.net'
+        // ,'live.staticflickr.com'   // si quiero agregar las fotos de flickr
+      ],
     },
   };
   
